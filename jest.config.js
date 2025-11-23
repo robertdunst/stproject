1cb2b08f9a4b6118a1274d8b2cd57e0e9dac4934
FelixHam <felix@hambi.net>
2025-11-23 13:35:42 +0200

    updated package.json and jest.config.js and added gitignore

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,18 @@
 module.exports = {
+  // test environment and the extensions
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testMatch: ['<rootDir>/tests/**/*.test.js']
+  setupFilesAfterEnv: ["jest-extended", "jest-chain"],
+
+  // we want coverage
+  collectCoverage: true,
+  coverageProvider: "v8",
+  coverageDirectory: "coverage",
+  coverageReporters: ["lcov", "text", "text-summary"],
+
+  // directory for our tests
+  roots: ["<rootDir>/tests"], 
+
+  // ignore the .internal files regarding test coverage reports
+  testPathIgnorePatterns: ["/node_modules/", "/.internal/"],
+  coveragePathIgnorePatterns: ["/node_modules/", "/.internal/"]
 };
