c6825932147f02044e58394441fdac07f6ba0066
Robert Dunst <dunstrobert99@gmail.com>
2025-11-24 21:34:50 +0200

    Change compact.test.js

--- tests/compact.test.js
+++ tests/compact.test.js
@@ -5,9 +5,10 @@ describe('compact', () => {
 
   // true+false values
   test('array with true and false values', () => {
-    expect(compact([0, 1, false, 2, '', 3])).toMatchObject([2, 3]);
-    expect(compact([0, 1, false, 2, '', 3])).toMatchObject([1, 2, 3]);
-    expect(compact([0, 1, true, 2, '', 3])).toEqual([ , true, 2, 3]);
+    expect(compact([1, 0, false, 2, '', 3])).toEqual([1, 2, 3]);
+    expect(compact([0, 1, false, 2, '', 3])).toEqual([1, 2, 3]);
+    expect(compact([1, 0, true, 2, '', 3])).toEqual([1 , true, 2, 3]);
+    expect(compact([0, 1, true, 2, '', 3])).toEqual([1 , true, 2, 3]);
   })
 
   // only true values
