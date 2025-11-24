a17f5bdb5498724c068cecfab61aa2314cd5b16e
Robert Dunst <dunstrobert99@gmail.com>
2025-11-24 22:00:22 +0200

    Change countBy.test.js

--- tests/countBy.test.js
+++ tests/countBy.test.js
@@ -10,10 +10,8 @@ describe('countBy', () => {
       { user: 'betty', active: true },
       { user: 'fred', active: false }
     ];
-    const result = countBy(users, value => value.active);
-    expect(result).toEqual({ true: 2, false: 1 });
+    expect(countBy(users, value => value.active)).toEqual({ true: 1, false: 0 });
   });
-
 
 
 })
