4c1c3aa88144716576f88a95c4e098d3c996f97b
Robert Dunst <dunstrobert99@gmail.com>
2025-11-24 21:56:14 +0200

    Change countBy.test.js

--- tests/countBy.test.js
+++ tests/countBy.test.js
@@ -1,2 +1,19 @@
 // tests for countBy function (countBy.js)
 import countBy from '../src/countBy.js';
+
+describe('countBy', () => {
+
+  // users
+  test('counts active and inactive users', () => {
+    const users = [
+      { user: 'barney', active: true },
+      { user: 'betty', active: true },
+      { user: 'fred', active: false }
+    ];
+    const result = countBy(users, value => value.active);
+    expect(result).toEqual({ true: 2, false: 1 });
+  });
+
+
+
+})
