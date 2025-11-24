b97a78f4539f02dd6491b71a44e2622da104647f
Robert Dunst <dunstrobert99@gmail.com>
2025-11-24 20:27:48 +0200

    Change compact.test.js

--- tests/compact.test.js
+++ tests/compact.test.js
@@ -5,35 +5,35 @@ describe('compact', () => {
 
   // true+false values
   test('array with true and false values', () => {
-    expect(compact([0, 1, false, 2, '', 3])).toBe([1, 2, 3]);
-    expect(compact([0, 1, true, 2, '', 3])).toBe([1, true, 2, 3]);
+    expect(compact([0, 1, false, 2, '', 3])).toEqual([1, 2, 3]);
+    expect(compact([0, 1, true, 2, '', 3])).toEqual([1, true, 2, 3]);
   })
 
   // only true values
   test('array with only true values', () => {
-    expect(compact([0, 1, 2, 3, 4, 5])).toBe([0, 1, 2, 3, 4, 5]);
+    expect(compact([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
   })
 
   // only false values
   test('array with only false values', () => {
-    expect(compact([0, "", false, null, undefined, NaN])).toBe([ ]);
+    expect(compact([0, "", false, null, undefined, NaN])).toEqual([ ]);
   })
 
   // empty array
   test('array that contains no values', () => {
-    expect(compact([ ])).toBe([ ]);
+    expect(compact([ ])).toEqual([ ]);
   })
 
   // nested array
   test('array that contains nested objects', () => {
-    expect(compact([0, [false], {a: 0}, {b: 1}, [1]])).toBe([[false], {a: 0}, {b: 1}, [1]]);
+    expect(compact([0, [false], {a: 0}, {b: 1}, [1]])).toEqual([[false], {a: 0}, {b: 1}, [1]]);
   })
 
   // ###
   test('###', () => {
-    expect(compact( )).toBe();
-    expect(compact(null)).toBe();
-    expect(compact(111)).toBe();
+    expect(compact( )).toEqual();
+    expect(compact(null)).toEqual();
+    expect(compact(111)).toEqual();
   })
 
 })
