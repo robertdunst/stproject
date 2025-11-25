f7aa4ebe45af3ac5d75007c7539da53ba78e16dd
Robert Dunst <dunstrobert99@gmail.com>
2025-11-25 17:13:48 +0200

    Change filter.test.js

--- tests/filter.test.js
+++ tests/filter.test.js
@@ -23,19 +23,19 @@ describe('filter', () => {
   // simple array but wrong filter
   test('returns empty array when no element matches the filter', () => {
     const data = [1, 2, 3, 4, 5, 6, 7, 8];
-    expect(filter(data, v => v > 10)).toEqual([]);
+    expect(filter(data, v => v > 10)).toEqual([[]]);
   });
 
   // empty array
   test('test empty array', () => {
-    expect(filter([], () => true)).toEqual([]);
+    expect(filter([], () => true)).toEqual([[]]);
   });
 
   // NaN, null, undefined
   test('test NaN, null and undefined', () => {
-    expect(filter(NaN, () => true)).toEqual([]);
-    expect(filter(null, () => true)).toEqual([]);
-    expect(filter(undefined, () => true)).toEqual([]);
+    expect(filter(NaN, () => true)).toEqual([[]]);
+    expect(filter(null, () => true)).toEqual([[]]);
+    expect(filter(undefined, () => true)).toEqual([[]]);
   });
 
   // 
