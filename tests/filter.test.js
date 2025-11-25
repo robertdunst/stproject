bc5afbfc1c111fa721e57636705d928644022b6e
Robert Dunst <dunstrobert99@gmail.com>
2025-11-25 15:32:53 +0200

    Change filter.test.js

--- tests/filter.test.js
+++ tests/filter.test.js
@@ -1,2 +1,17 @@
 // tests for filter function (filter.js)
 import filter from '../src/filter.js';
+
+describe('filter', () => {
+
+  // basic case
+  test('###', () => {
+    const users = [
+      { user: 'barney', active: true },
+      { user: 'fred', active: false }
+    ];
+    expect(filter(users, ({ active }) => active)).toEqual([
+      { user: 'barney', active: true }
+    ]);
+  });
+
+})
