e9bc08463caf75c4b117498fe4919a67ac689160
Robert Dunst <dunstrobert99@gmail.com>
2025-11-25 11:58:15 +0200

    Change countBy.test.js

--- tests/countBy.test.js
+++ tests/countBy.test.js
@@ -3,7 +3,7 @@ import countBy from '../src/countBy.js';
 
 describe('countBy', () => {
 
-  // users
+  // ToDo: Defo not the correct expected value
   test('counts active and inactive users', () => {
     const users = [
       { user: 'barney', active: true },
@@ -12,6 +12,25 @@ describe('countBy', () => {
     ];
     expect(countBy(users, value => value.active)).toEqual({ true: 1, false: 0 });
   });
+
+  // empty array
+  test('counts elements of an empty array', () => {
+    expect(countBy([], x => x)).toEqual({});
+  });
+
+  // string length
+  test('counts by string length', () => {
+    const words = ['a', 'bb', 'ccc', 'dddd', 'dddd'];
+    expect(countBy(words, w => w.length)).toEqual({ '1': 1, '2': 1, '3': 1, '4': 2 });
+  });
+
+  // even/odd
+  test('counts numbers by even/odd', () => {
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 7, 7];
+    expect(countBy(numbers, n => (n % 2 === 0 ? 'even' : 'odd'))).toEqual({ odd: 5, even: 2 });
+  });
+
+
 
 
 })
