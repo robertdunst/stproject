2768cb69a72ff7428c6139eb1c616d04603df4b6
Robert Dunst <dunstrobert99@gmail.com>
2025-11-25 16:51:08 +0200

    Change difference.test.js

--- tests/difference.test.js
+++ tests/difference.test.js
@@ -12,6 +12,23 @@ describe('difference', () => {
     expect(difference([3, 2, 4, 1], [2, 4])).toEqual([3, 1]);
   });
 
+  // data types
+  test('mixed data types', () => {
+    expect(difference([1, 'abc', true, false], ['abc', false])).toEqual([1, true]);
+  });
+
+  // repeated values
+  test('removes all occurrences of repeated values', () => {
+    expect(difference([1, 2, 2, 2, 3, 3, 4], [2, 3])).toEqual([1, 4]);
+  });
+
+  // empty arrays
+  test('test empty arrays', () => {
+    expect(difference([], [1, 2])).toEqual([]);
+    expect(difference([1, 2], [])).toEqual([1, 2]);
+    expect(difference([], [])).toEqual([]);
+  });
+
   // multiple arrays
   test('tests multiple arrays', () => {
     expect(difference([1, 2, 3, 4], [1], [3])).toEqual([2, 4]);
@@ -47,5 +64,22 @@ describe('difference', () => {
     expect(difference('abc', ['a'])).toEqual([]);
   });
 
+  // non array
+  test('second argument non array', () => {
+    expect(difference([1, 2, 3], null)).toEqual([1, 2, 3]);
+    expect(difference([1, 2, 3], 2)).toEqual([1, 2, 3]);
+  });
+
+  // one argument
+  test('test without second argument', () => {
+    expect(difference([1, 2, 3])).toEqual([1, 2, 3]);
+  });
+
+  // nested arrays
+  test('nested arrays compared by reference', () => {
+    const a = [1];
+    const b = [1];
+    expect(difference([a, b], [a])).toEqual([b]);
+  });
 
 })
